e3fd5bd94eff9b6f49479bebd82654a69f4702bf
VLADIMIR <veselov2100@mail.ru>
2021-09-13 15:15:09 +0300

    Complete

--- CPP_OOP_HW6.cpp
+++ CPP_OOP_HW6.cpp
@@ -1,20 +1,41 @@
-// CPP_OOP_HW6.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cstdlib>
+
+//Task 1 ============================================================
+
+int getInt()
+{
+    int input = 0;
+    while (true)
+    {
+        std::cout << "Enter your number: ";
+        std::cin >> input;
+        if (std::cin) return input;
+        else
+        {
+            system("cls");
+            std::cerr << "Incorrect format!" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(10000, '\n');           
+        }
+    }
+}
+
+//Task 2 =============================================================
+
+std::ostream& endll(std::ostream &stream)
+{
+    stream << "\n\n";
+    stream.clear();
+    return stream;
+}
+
+//Task 3 =============================================================
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int result = getInt();
+    std::cout << "Your number is " << result << endll << "Task 2 test";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
